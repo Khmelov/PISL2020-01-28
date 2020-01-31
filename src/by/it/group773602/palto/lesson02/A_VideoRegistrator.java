2720447f255bd7d43806a3469f1b14f381bcea52
Lena Palto <palto_elena@tut.by>
2020-01-31 12:21:33 +0300

    Lesson2 A B

--- src/by/it/group773602/palto/lesson02/A_VideoRegistrator.java
+++ src/by/it/group773602/palto/lesson02/A_VideoRegistrator.java
@@ -1,6 +1,7 @@
 package by.it.group773602.palto.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 даны события events
@@ -13,32 +14,37 @@ import java.util.List;
 public class A_VideoRegistrator {
 
     public static void main(String[] args) {
-        A_VideoRegistrator instance=new A_VideoRegistrator();
-        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
+        A_VideoRegistrator instance = new A_VideoRegistrator();
+        double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts = instance.calcStartTimes(events, 1); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
     }
+
     //модификаторы доступа опущены для возможности тестирования
-    List<Double> calcStartTimes(double[] events, double workDuration){
+    List<Double> calcStartTimes(double[] events, double workDuration) {
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
-        List<Double> result;
-        result = new ArrayList<>();
-        int i=0;                              //i - это индекс события events[i]
+        List<Double> result = new ArrayList<>();
+        int i = 0;                              //i - это индекс события events[i]
+
         //комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
         //подготовка к жадному поглощению массива событий
-        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
+        Arrays.sort(events);//hint: сортировка Arrays.sort обеспечит скорость алгоритма
         //C*(n log n) + C1*n = O(n log n)
 
-        //пока есть незарегистрированные события
-        //получим одно событие по левому краю
-        //и запомним время старта видеокамеры
-        //вычислим момент окончания работы видеокамеры
-        //и теперь пропустим все покрываемые события
-        //за время до конца работы, увеличивая индекс
 
+        double leftEvent;
+        double endTime;
+        while (i < events.length) { //пока есть незарегистрированные события
+            leftEvent = events[i]; //получим одно событие по левому краю
+            result.add(leftEvent); //и запомним время старта видеокамеры
+            endTime = leftEvent + workDuration;//вычислим момент окончания работы видеокамеры
+            i++;
+            while (i < events.length && events[i] <= endTime) {//и теперь пропустим все покрываемые события
+                i++;                                        //за время до конца работы, увеличивая индекс
+            }
 
-
-        return result;                        //вернем итог
+        }
+        return result;  //вернем итог
     }
 }
--- src/by/it/group773602/palto/lesson02/B_Sheduler.java
+++ src/by/it/group773602/palto/lesson02/B_Sheduler.java
@@ -1,6 +1,8 @@
 package by.it.group773602.palto.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 /*
 даны интервальные события events
@@ -11,7 +13,7 @@ import java.util.List;
 
 public class B_Sheduler {
     //событие у аудитории(два поля: начало и конец)
-    static class Event {
+    static class Event implements Comparable<Event> {
         int start;
         int stop;
 
@@ -21,22 +23,28 @@ public class B_Sheduler {
         }
 
         @Override
+        public int compareTo(Event e) {
+            return (stop - e.stop);
+        }
+
+        @Override
         public String toString() {
-            return "("+ start +":" + stop + ")";
+            return "(" + start + ":" + stop + ")";
         }
     }
 
+
     public static void main(String[] args) {
         B_Sheduler instance = new B_Sheduler();
-        Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
-                new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
-                new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
-                new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
-                new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
-                new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
+        Event[] events = {new Event(0, 3), new Event(0, 1), new Event(1, 2), new Event(3, 5),
+                new Event(1, 3), new Event(1, 3), new Event(1, 3), new Event(3, 6),
+                new Event(2, 7), new Event(2, 3), new Event(2, 7), new Event(7, 9),
+                new Event(3, 5), new Event(2, 4), new Event(2, 3), new Event(3, 7),
+                new Event(4, 5), new Event(6, 7), new Event(6, 9), new Event(7, 9),
+                new Event(8, 9), new Event(4, 6), new Event(8, 10), new Event(7, 10)
         };
 
-        List<Event> starts = instance.calcStartTimes(events,0,10);  //рассчитаем оптимальное заполнение аудитории
+        List<Event> starts = instance.calcStartTimes(events, 0, 10);  //рассчитаем оптимальное заполнение аудитории
         System.out.println(starts);                                 //покажем рассчитанный график занятий
     }
 
@@ -48,10 +56,35 @@ public class B_Sheduler {
         List<Event> result;
         result = new ArrayList<>();
         //ваше решение.
+        Arrays.sort(events);
 
+        int i = 0;
+        while (i < events.length - 1) {
+            result.add(events[i]);
 
-
-
+            int end = events[i++].stop;
+            while (events[i].start < end && events[i].stop < to) {
+                i++;
+                if (events[i].start < end && events[i].stop == to )
+                    return result;
+            }
+        }
+//        List<Event> listEvents = Arrays.asList(events);
+//
+//        listEvents.sort(Comparator.comparing(event -> event.stop));
+//
+//        for (Event event : events) {
+//            if (result.isEmpty())
+//                result.add(event);
+//            else if (event.start == result.get(result.size() - 1).start && event.stop < result.get(result.size() - 1).stop) {
+//                result.remove(result.get(result.size() - 1));
+//                result.add(event);
+//            }
+//            else if (event.start >= result.get(result.size() - 1).stop) {
+//                result.add(event);
+//            }
+//        }
+//
 
 
         return result;                        //вернем итог
