afe40d83891639277c932226dfcc7655f06001f6
Skachko Gleb <skachko42@gmail.com>
2020-05-05 17:51:04 +0300

    lab5

---
+++ projects-for-lab5,6,7/group773601/skachko/lab5/java/classes/City.java
@@ -0,0 +1,24 @@
+package classes;
+
+import javax.xml.bind.annotation.*;
+
+@XmlType
+@XmlEnum
+public enum City {
+    @XmlEnumValue("Minsk") Minsk("Minsk"),
+    @XmlEnumValue("Grodno") Grodno("Grodno"),
+    @XmlEnumValue("Gomel") Gomel("Gomel"),
+    @XmlEnumValue("Vitebsk") Vitebsk("Vitebsk"),
+    @XmlEnumValue("Brest") Brest("Brest");
+
+    private String value;
+
+    City(String s){
+        this.value = s;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
